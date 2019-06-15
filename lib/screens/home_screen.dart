cf2a82c808bff9af6d812cae7fc91c93e3a3a71c
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-06-15 13:28:17 +0200

    add basic ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
+  final int counter = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -8,7 +9,41 @@ class HomeScreen extends StatelessWidget {
         title: Text('Flutter Redux'),
       ),
       body: Container(
-        child: Center(child: Text('Home Screen')),
+        width: double.infinity,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              counter.toString(),
+              style: TextStyle(
+                fontSize: 50.0,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                RaisedButton(
+                  child: Icon(Icons.remove),
+                  onPressed: () {
+                    print('Decrement');
+                  },
+                ),
+                SizedBox(
+                  width: 10.0,
+                ),
+                RaisedButton(
+                  child: Icon(Icons.add),
+                  onPressed: () {
+                    print('Increment');
+                  },
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
