2b0d2668a18ba698d1541c352543f4c2df69799a
Milos Dzeletovic <milos.dzeletovic@consulteer.com>
2019-06-16 19:32:33 +0200

    fix a bug

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
 import './redux/store.dart';
+import './redux/models/app_state.dart';
 import './screens/home_screen.dart';
 
 void main() => runApp(MyApp());
@@ -9,7 +10,7 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return StoreProvider(
+    return StoreProvider<AppState>(
       store: store,
       child: MaterialApp(
         theme: ThemeData.dark(),
--- lib/screens/counter.dart
+++ lib/screens/counter.dart
@@ -1,14 +1,32 @@
 import 'package:flutter/material.dart';
+import 'package:redux/redux.dart';
+import 'package:flutter_redux/flutter_redux.dart';
+
+import '../redux/models/app_state.dart';
 
 class Counter extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Text(
-      '0',
-      style: TextStyle(
-        fontSize: 50.0,
-        fontWeight: FontWeight.w600,
-      ),
-    );
+    return StoreConnector<AppState, _ViewModel>(
+        converter: _ViewModel.fromStore,
+        builder: (BuildContext context, _ViewModel viewModel) {
+          return Text(
+            viewModel.counter.toString(),
+            style: TextStyle(
+              fontSize: 50.0,
+              fontWeight: FontWeight.w600,
+            ),
+          );
+        });
+  }
+}
+
+class _ViewModel {
+  final int counter;
+
+  _ViewModel({@required this.counter});
+
+  static _ViewModel fromStore(Store<AppState> store) {
+    return _ViewModel(counter: store.state.counter);
   }
 }
